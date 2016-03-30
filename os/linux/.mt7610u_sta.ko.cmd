891ef2bc4397ada23744a428ab19cc9cec43c145
Jeff Williams <jeff.williams@here.com>
2016-03-29 20:10:20 -0700

    cleanup incorrectly added files

--- os/linux/.mt7610u_sta.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/jwilliams/mt7610u-linksys-ae6000-wifi-fixes/os/linux/mt7610u_sta.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/jwilliams/mt7610u-linksys-ae6000-wifi-fixes/os/linux/mt7610u_sta.ko /home/jwilliams/mt7610u-linksys-ae6000-wifi-fixes/os/linux/mt7610u_sta.o /home/jwilliams/mt7610u-linksys-ae6000-wifi-fixes/os/linux/mt7610u_sta.mod.o
